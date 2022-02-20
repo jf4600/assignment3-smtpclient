73368ac4e4fe8807fa660181b6162033942a34c7
jf4600 <98861587+jf4600@users.noreply.github.com>
2022-02-19 19:08:22 -0500

    try try again

--- solution.py
+++ solution.py
@@ -54,8 +54,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Message ends with a single period, send message end and handle server response.
     # Fill in start
-    clientSocket.send(msg.encode())
-    clientSocket.send(endmsg.encode())
+    #clientSocket.send(msg.encode())
+    #clientSocket.send(endmsg.encode())
     # Fill in end
 
     # Send QUIT command and handle server response.
